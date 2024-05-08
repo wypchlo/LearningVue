5685613a06ce700f9760a83f0cc7a63f4cc1f139
= <=>
2024-05-08 13:54:40 +0200

    Day 2 Reflex game - added a color indicator for score

--- reflex_game/src/components/ReflexResult.vue
+++ reflex_game/src/components/ReflexResult.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 id="result">You clicked the input in {{ reactionTime }}ms</h1>
+  <h1 id="result" ref="result"> You clicked the input in {{ reactionTime }}ms </h1>
 </template>
 
 <script>
@@ -8,11 +8,9 @@ export default {
   props: [
     "reactionTime"
   ],
+  mounted() { 
+    let score = (this.reactionTime - 200) / 400;
+    this.$refs.result.style.color = 'rgb(' + (score * 255) + ', ' + (-score * 255 + 255) + '0, 0)'; 
+  }
 };
 </script>
-
-<style>
-#result {
-  color: green;
-}
-</style>

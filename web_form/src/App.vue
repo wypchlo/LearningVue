711bbf863de1d29f5c658bd93d6aca06f073c94a
wypchlo <deron.pl.yt@gmail.com>
2024-05-09 10:57:51 +0200

    Day 3 Created a basic form design using tailwind

--- web_form/src/App.vue
+++ web_form/src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <StyledForm title="Welcome!">
-      <label><p> Name </p><input type="text"></label>
-      <label><p> Surname </p><input type="text"></label>
-      <label><p> Email </p><input type="email"></label>
-  </StyledForm>
+  <button class="focus:bg-black">Test</button>
+  <PopupBox>
+    <h1 class="py-6 w-64 border-b border-stone-200 font-bold text-lg text-stone-600"> welcome </h1>
+    <form class="w-full font-light flex flex-col items-center justify-center mb-4" id="signupform">
+      <label class="flex flex-col w-3/4 h-14 justify-between my-2"> <p class="text-stone-600 font-normal"> name </p> <input class="border-b border-stone-200 outline-none text-center rounded-md" type="text" ref="name" placeholder> </label>
+      <label class="flex flex-col w-3/4 h-14 justify-between my-2"> <p class="text-stone-600 font-normal"> surname </p> <input class="border-b border-stone-200 outline-none text-center rounded-md" type="text" ref="surname" placeholder> </label>
+      <label class="flex flex-col w-3/4 h-14 justify-between my-2"> <p class="text-stone-600 font-normal"> email </p> <input class="border-b border-stone-200 outline-none text-center rounded-md" type="email" ref="email" placeholder> </label>
+      <label class="flex flex-col w-3/4 h-14 justify-between my-2"> <p class="text-stone-600 font-normal"> password </p> <input class="border-b border-stone-200 outline-none text-center rounded-md" type="password" ref="password" placeholder> </label>
+    </form>
+  </PopupBox>
 </template>
 
 <script>
-import StyledForm from './components/StyledForm.vue';
+import PopupBox from './components/PopupBox.vue';
 
-export default {
-  name: 'App',
+export default{
   components: {
-    StyledForm 
+    PopupBox
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif; 
-  text-align: center;
-  color: #4F4746;
-  margin-top: 60px;
-}
+  #signupform input:not(:placeholder-shown), #signupform input:focus{
+    @apply border;
+  }
 </style>
---
+++ web_form/src/components/PopupBox.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-black/[.5] absolute w-full h-full top-0 flex justify-center items-center">
+        <div class="w-fit h-fit rounded-xl bg-white text-center">
+            <slot/>
+        </div>
+    </div>
+</template>

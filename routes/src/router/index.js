197f7ea52e13c1c4d1e92b65bb96c834b095c34a
wypchlo <deron.pl.yt@gmail.com>
2024-05-10 07:42:24 +0200

    Day 4 - routes 404 page and router links with named

--- routes/src/router/index.js
+++ routes/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import JobsView from '../views/JobsView.vue'
+import JobView from '../views/JobView.vue'
+import PageNotFound from '../views/PageNotFound.vue'
 
 const routes = [
   {
@@ -12,6 +15,22 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: JobsView
+  },
+  {
+    path: '/jobs/:id',
+    name: 'job',
+    component: JobView
+  },
+  // catchall
+  {
+    path: '/:catchAll(.*)',
+    name: '404',
+    component: PageNotFound
   }
 ]
 
---
+++ routes/src/views/JobsView.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-link :to="{ name: 'job', params: { id: job.id } }" v-for="job in jobs" :key="job.id">
+        {{ job.title }}
+    </router-link>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                jobs: [
+                    {title: "Job1", id: 1},
+                    {title: "Job2", id: 2},
+                    {title: "Job3", id: 3}
+                ]
+            }
+        }
+    }
+</script>

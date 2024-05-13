b54728414b382ac78ddb4b633b5febdb87f6d8da
wypchlo <deron.pl.yt@gmail.com>
2024-05-13 11:31:09 +0200

    Project planner - extended functionality and added a form Stopped counting the days

--- project-planner/src/components/ProjectList.vue
+++ project-planner/src/components/ProjectList.vue
@@ -1,10 +1,25 @@
 <template> 
-    <ul class="flex flex-col gap-6 items-center py-6">
-        <li @click="expand(project.id)" class="bg-white rounded-lg p-6 min-w-96 w-3/4 border-b-4 select-none cursor-pointer" :class="project.completed ? 'border-lime-800'  : 'border-red-800'" v-for="project in projects" :key="project">
-            <h1 class="text-2xl font-teachers tracking-wide font-medium text-stone-500 text-center"> {{ project.name }} </h1>
-            <div v-if="expanded.includes(project.id)" class="text-center text-stone-400 font-teachers"> {{ project.description }} </div>
-        </li>
-    </ul> 
+    <div class="flex flex-col gap-6 items-center py-6">
+        <button @click="expand(project.id)" class="bg-white rounded-lg p-6 min-w-96 w-3/4 border-b-4 select-none cursor-pointer" :class="project.completed ? 'border-lime-800'  : 'border-red-800'" v-for="(project, index) in projects" :key="project">
+            <div id="container" class="flex justify-between">
+                <div id="content" class="text-left" v-if="editing != project.id">
+                    <h1 class="text-2xl font-teachers tracking-wide font-medium text-stone-500"> {{ project.name }} </h1>
+                    <div v-if="expanded.includes(project.id)" class="text-stone-400 font-teachers"> {{ project.description }} </div>
+                </div>
+
+                <div id="contentEditing" @keyup.enter="saveEdit(project.id, index)" class="text-left" v-else>
+                    <h1 class="text-2xl font-teachers tracking-wide font-medium text-stone-500"> <input class="outline-none" v-model="projects[index].name"> </h1>
+                    <div v-if="expanded.includes(project.id)" class="text-stone-400 font-teachers"> <input class="w-96 outline-none" v-model="projects[index].description"> </div>
+                </div>
+
+                <div id="icons" class="h-full flex my-auto gap-3">
+                    <button @click.stop="editProject(project.id)"> <span class="material-icons text-stone-400 text-2xl hover:text-stone-500">edit</span> </button>
+                    <button @click.stop="deleteProject(project.id, index)"> <span class="material-icons text-stone-400 text-2xl hover:text-stone-500">delete</span> </button>
+                    <button @click.stop="completeProject(project.id, index)"> <span class="material-icons text-stone-400 text-2xl hover:text-stone-500 font-bold">done</span> </button>
+                </div>
+            </div>
+        </button>
+    </div> 
 </template>
 
 <script>
@@ -14,7 +29,9 @@
         data() {
             return {
                 expanded: [],
-                projects: []
+                projects: [],
+                editing: null,
+                uri: 'http://localhost:3000/projects/'
             }
         },
         methods: {
@@ -41,8 +58,33 @@
                 this.expanded = [];
             },
             expand(id) {
+                if(this.editing == id) return;
                 if(this.expanded.includes(id)) this.expanded.splice(this.expanded.indexOf(id), 1);
                 else this.expanded.push(id);
+            },
+            editProject(id) {
+                if(!this.expanded.includes(id)) this.expanded.push(id);
+                this.editing = id;
+            },
+            saveEdit(id, index) {
+                this.editing = null;
+                fetch(this.uri+id, { 
+                    method: 'PATCH',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ name: this.projects[index].name, description: this.projects[index].description })
+                })
+            },
+            deleteProject(id, index) {
+                fetch(this.uri+id, {method: 'DELETE'});
+                this.projects.splice(index, 1);
+            },
+            completeProject(id, index) {
+                fetch(this.uri+id, { 
+                    method: 'PATCH',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ completed: !this.projects[index].completed })
+                });
+                this.projects[index].completed = !this.projects[index].completed;
             }
         },
         mounted() {
--- project-planner/src/views/AddProjectView.vue
+++ project-planner/src/views/AddProjectView.vue
@@ -1,3 +1,28 @@
 <template>
- <div> Add project </div>
+    <div class="flex justify-center w-full font-teachers">
+        <div id="container" class="w-fit h-fit rounded-xl bg-white text-center">
+            <h1 class="text-2xl font-teachers tracking-wide font-medium text-stone-500 px-36 py-6 border-stone-300 border-b-2"> add project </h1>
+            <form @submit.prevent="handleSubmit" class="w-full font-light flex flex-col items-center justify-center mb-4" id="signupform">
+            <label class="flex flex-col w-3/4 h-14 justify-between my-6"> <p class="my-2 font-semibold font-teachers text-xl text-stone-500 transition-colors select-none cursor-pointer"> name </p> <input v-model="name" class="border-b-2 border-stone-300 placeholder-stone-400 text-stone-500 outline-none text-center rounded-md" type="text" ref="name" placeholder="vue project"> </label>
+            <label class="flex flex-col w-3/4 h-fit justify-between my-2"> <p class="my-2 font-semibold font-teachers text-xl text-stone-500 transition-colors select-none cursor-pointer"> description </p> <textarea v-model="surname" class="bg-stone-100 align-bottom border-b-2 border-stone-300 placeholder-stone-400 text-stone-500 outline-none text-center rounded-md" type="text" ref="surname" placeholder="a simple vue project"></textarea> </label>
+            
+            <button @submit="submitForm()" class="bg-stone-200 text-stone-600 rounded-full text-2xl font-teachers tracking-wide font-medium mt-6 mb-2 hover:bg-stone-300" type="submit"> <h1 class="w-full px-6 py-1"> confirm </h1> </button>
+            </form>        
+        </div>   
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            submitForm() {
+                
+            }
+        }
+    }
+</script>

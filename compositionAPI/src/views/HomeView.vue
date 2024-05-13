813c728afca73690dea61c5fc4db7caa1d5cb532
wypchlo <deron.pl.yt@gmail.com>
2024-05-13 14:43:31 +0200

    Added a simple search functionality to the CompositionAPI project Day 5

--- compositionAPI/src/views/HomeView.vue
+++ compositionAPI/src/views/HomeView.vue
@@ -1,3 +1,28 @@
 <template>
-    Home view
+    <div class="flex justify-center py-10">
+        <label class="text-center"> <p> Search </p> <input v-model="searchTerm" type="text" class="border"> </label>
+    </div>
+
+    <ul class="text-center">
+        <li v-for="item in filteredItems" :key="item"> {{ item }} </li>
+    </ul>
 </template>
+
+<script>
+    import {ref, computed} from 'vue';
+
+    export default
+    {
+        setup()
+        {
+            const items = ref(['pipeyard', 'chimney canopy', 'the exterior', 'sky islands', 'five pebbles', 'outskirts']);
+            const searchTerm = ref(null);
+            const filteredItems = computed(() => {
+                return items.value.filter(item => item.includes(searchTerm.value))
+            }) 
+
+
+            return {items, searchTerm, filteredItems};
+        }
+    }
+</script>
